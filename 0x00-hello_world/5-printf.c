d03384f50079d9dc6578c728f20e6b73935bf789
SlimzyDeOriginal <desurvivor2012@gmail.com>
2023-02-19 09:16:07 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - print the text using printf on a new line
+ *
+ *Return: 0 (Successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
