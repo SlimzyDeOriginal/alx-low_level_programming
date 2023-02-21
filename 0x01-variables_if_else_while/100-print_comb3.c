84a3bae21157ed8140c6fff8c20b0cd30672138a
SlimzyDeOriginal <desurvivor2012@gmail.com>
2023-02-20 19:29:43 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print a combination of numbers'
+ *Return: always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
