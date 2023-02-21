5b820a91b8299789b21d1b83bd4ea39833f3da51
SlimzyDeOriginal <desurvivor2012@gmail.com>
2023-02-20 18:23:29 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print all digits of base 10'
+ *Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
