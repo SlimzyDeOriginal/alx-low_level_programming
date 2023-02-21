12afe05c28a3b905543343d2bfd765a678828666
SlimzyDeOriginal <desurvivor2012@gmail.com>
2023-02-20 18:59:13 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print all numbers base 16 in lowercase'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (b = 97; b <= 102; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
